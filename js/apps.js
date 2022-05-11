718482c609e67bba83507f220a9d0982afca171e
boburbekburkhonov <zafaroviccc@gmail.com>
2022-05-11 20:16:09 +0500

    feat: js bg

--- js/apps.js
+++ js/apps.js
@@ -4,3 +4,9 @@ let elHeader = document.querySelector(".header");
 elHamburger.addEventListener("click", () => {
   elHeader.classList.toggle("header--open")
 } )
+
+const elHeaderBg = document.querySelector(".header__bg")
+
+elHeaderBg.addEventListener("click", () => {
+  elHeader.classList.toggle("header--open")
+})
